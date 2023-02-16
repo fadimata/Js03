849c6b25f972cf0b319ee6a0eb5910667ab5d004
fadimata <115995864+fadimata@users.noreply.github.com>
2023-02-16 16:44:48 +0100

    Add files via upload

---
+++ Js03/displayBookInfo.js
@@ -0,0 +1,14 @@
+const book = {
+    titre: "L'enfant noir ",
+    auteur: "camara laye",
+    année: 1953
+};
+function afficherInfoLivre() {
+    console.log(`titre: ${ book.titre}`);
+    console.log(`auteur: ${ book.auteur}`);
+    console.log(`année: ${ book.année}`);
+
+    
+};
+afficherInfoLivre();
+
---
+++ Js03/displayPersonInfo.js
@@ -0,0 +1,9 @@
+const personne ={
+    nom : "Mariam",
+    age : "30", 
+}
+function displayPeresonInfo (personne){
+    console.log("Non:" + personne.nom +"\nAge:" + personne.age);
+}
+displayPeresonInfo(personne);
+
